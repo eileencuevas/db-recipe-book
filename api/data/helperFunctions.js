d770cdc18a0538bf2a9386206b6b710cce794732
steelworx <eileencuevas62@gmail.com>
2019-01-18 00:00:49 -0500

    created helper functions in helperFunctions.js

---
+++ api/data/helperFunctions.js
@@ -0,0 +1,66 @@
+const knex = require('knex');
+const knexConfig = require('../../knexfile');
+
+const db = knex(knexConfig.development);
+
+const getRecipeIngredients = () => {
+    return db('RecipeIngredients');
+}
+
+const getDishes = () => {
+    return db('dishes');
+}
+
+const getDish = (id) => {
+    const dish = db('dishes')
+        .where({ id : id });
+
+    const recipes = db('recipes')
+        .select('name', 'description')
+        .where({ 'dish_id' : id })
+    
+    return Promise
+        .all([dish, recipes])
+        .then(results => {
+            console.log(results);
+            let [dish, recipes] = results;
+            return { dish: dish.name, id: dish.id, recipes: recipes };
+        })
+}
+
+const addDish = (dish) => {
+    return db('dishes').insert(dish);
+}
+
+const getRecipes = () => {
+    return db('recipes');    
+}
+
+const addRecipe = (req, res) => {
+    return db('recipes').insert(req.body);
+}
+
+const getShoppingList = (id) => {
+    const recipe = db('recipes')
+        .where({ id : id });
+
+    const ingredients = db('recipeIngredients as ri')
+        .join('ingredients as in', { 'in.id': 'ri.ingredientId' })
+        .select('name', 'quantity')
+        .where({ 'ri.recipeId': id });
+
+    return Promise
+        .all([recipe, ingredients])
+        .then(results => {
+            console.log(results);
+            let [recipe, ingredients] = results;
+            return { 
+                recipe: recipe[0].name, 
+                id: recipe[0].id, 
+                description: recipe[0].description,
+                ingredients: ingredients
+            };
+        })
+}
+
+module.exports = { getRecipeIngredients, getDishes, addDish, getDish, getRecipes, addRecipe, getShoppingList };
