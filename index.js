8598c684ad1baf2e4f955ef4dbf8bc4f8c662be9
steelworx <eileencuevas62@gmail.com>
2019-01-18 00:01:40 -0500

    created simple endpoints in index.js for testing purposes

--- index.js
+++ index.js
@@ -1,22 +1,84 @@
 const express = require('express');
 const morgan = require('morgan');
-const knex = require('knex');
 
-const knexConfig = require('./knexfile');
-const db = knex(knexConfig.development);
+const helpers = require('./api/data/helperFunctions');
 
 const server = express();
 
 server.use(express.json());
 server.use(morgan('short'));
 
-server.get(`/api/:table`, (req, res) => {
-    db(req.params.table)
-        .then(thing => {
-            res.status(200).json(thing);
+// server.get('/', (req, res) => {
+//     db.getRecipeIngredients()
+//         .then(thing => {
+//             res.status(200).json(thing);
+//         })
+//         .catch(error => {
+//             res.status(500).json(error);
+//         });
+// })
+
+server.get(`/api/dishes/:id`, (req, res) => {
+    helpers.getDish(req.params.id)
+        .then(dish => {
+            if (dish) {
+                res.status(200).json(dish);
+            } else {
+                res.status(404).json({ error: 'Could not find dish with specified ID.' });
+            }
         })
         .catch(() => {
-            res.status(500).json({ error: `Could not retrieve data for ${table}.` });
+            res.status(500).json({ error: `Could not retrieve data for dishes.` });
+        });
+})
+
+server.get(`/api/dishes`, (req, res) => {
+    helpers.getDishes()
+        .then(dishes => {
+            res.status(200).json(dishes);
+        })
+        .catch(() => {
+            res.status(500).json({ error: `Could not retrieve data for dishes.` });
+        });
+})
+
+server.post(`/api/dishes`, (req, res) => {
+    helpers.addDish(req.body)
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(() => {
+            res.status(500).json({ error: `Could not add data for dishes.` });
+        });
+})
+
+server.get(`/api/recipes/:id/shopping-list`, (req, res) => {
+    helpers.getShoppingList(req.params.id)
+        .then(shoppingList => {
+            res.status(200).json(shoppingList);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+})
+
+server.get(`/api/recipes`, (req, res) => {
+    helpers.getRecipes(res)
+        .then(recipe => {
+            res.status(200).json(recipe);
+        })
+        .catch(() => {
+            res.status(500).json({ error: `Could not retrieve data for recipes.` });
+        });
+})
+
+server.post(`/api/recipes`, (req, res) => {
+    helpers.addRecipe(req, res)
+        .then(id => {
+             res.status(201).json(id);
+        })
+        .catch(() => {
+            res.status(500).json({ error: `Could not add data for recipes.` });
         });
 })
 
